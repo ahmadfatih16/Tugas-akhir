6b0f78c9c0996eedd6e654cb0995b9470796b1fc
ahmadfatih16 <152517428+ahmadfatih16@users.noreply.github.com>
2025-11-20 13:59:00 +0700

    menambahkan isi file index.ts agar dianggap sebagai modul (sebelumnya kosong)

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+export * from './types';
+export * from './config';
+export * from './linter';
+export * from './scanner';
+export * from './converter';
+export * from './validators';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello, Naming Linter!");
+export * from './types';
+export * from './config';
+export * from './linter';
+export * from './scanner';
+export * from './converter';
+export * from './validators';
